931a4252b13379ce0e2302abb7ba7aa97479b66b
Giulian Drimba <giuliandrimba@gmail.com>
2016-06-19 01:42:06 -0300

    Iris

--- src/sketch.js
+++ src/sketch.js
@@ -1,16 +1,44 @@
 var Line = require("./sketch/line")
+var noise = require("@giuliandrimba/noise");
+var calc = require("2pi-calc");
 
 var ctx = undefined;
 var env = undefined;
 var line = undefined;
+var colorNoise = 0;
+var lines = [];
 
 exports.setup = function(_ctx, _env){
   ctx = _ctx;
   env = _env;
 
   background("#fff");
-  line = new Line({x:100, y:0},{x:300, y:ctx.canvas.height}, {numPoints:100});
-  line.draw(ctx);
+
+  var numLines = 100 * (ctx.canvas.width / 1024)
+  var radius = 300 * (ctx.canvas.width / 1024)
+  var angleStep = 360 / numLines
+  var angle = 0;
+  var center = {x:ctx.canvas.width / 2 - 10, y:ctx.canvas.height / 2 - 10};
+
+  for(var j = 0; j < numLines; j++) {
+    colorNoise += 0.0001;
+    var color = noise(colorNoise);
+    var p1 = {
+      x:center.x + (Math.cos(angle) * radius),
+      y:center.y + (Math.sin(angle) * radius)
+    }
+
+    var p2 = {
+      x:center.x - (Math.cos(angle) * radius),
+      y:center.y - (Math.sin(angle) * radius)
+    }
+
+    var l = new Line(p1,p2, {numPoints:10, color:color, step:angleStep, origin:center})
+    l.draw(ctx);
+
+    angle += angleStep;
+  }
+  env.done()
 }
 
 exports.draw = function() {
--- src/sketch/line.js
+++ src/sketch/line.js
@@ -1,14 +1,17 @@
 var calc = require("2pi-calc");
 var Vector = require("2pi-vector");
+var noise = require("@giuliandrimba/noise");
 
 function Line(pointA, pointB, style){
   this.pointA = pointA;
   this.pointB = pointB;
   this.style = style;
   this.points = [];
+  this.noise = Math.random();
 }
 
 Line.prototype.draw = function(ctx){
+
   var dist = calc.dist(this.pointA.x, this.pointA.y, this.pointB.x, this.pointB.y);
   var direction = Vector.new();
   direction.x = this.pointB.x - this.pointA.x;
@@ -16,28 +19,49 @@ Line.prototype.draw = function(ctx){
   direction = Vector.normalize(direction)
   var steps = dist / this.style.numPoints;
 
-  ctx.strokeStyle = "#000"
+  console.log(this.style.color)
+  ctx.strokeStyle = "rgba(0,0,0, "+this.style.color + ")"
+  ctx.lineWidth = ctx.canvas.width / 1024
   ctx.beginPath()
+
   ctx.moveTo(this.pointA.x, this.pointA.y)
 
-  for(var i = 0; i <  2 + this.style.numPoints; i++) {
+  for(var i = 1; i < this.style.numPoints - 1; i++) {
 
+    this.noise += 0.1;
     var d = Vector.copy(direction)
+    var numNoise = noise(this.noise) * 300 * (ctx.canvas.width / 1024)
+    this.noise += 0.1;
+    var numNoise2 = -noise(this.noise) * 300 * (ctx.canvas.width / 1024)
     d = Vector.mult(d, steps * i);
     var p = Vector.add(this.pointA, d);
+
+    if(p.x <= this.style.origin.x && p.y <= this.style.origin.y) {
+      p.x -= numNoise + numNoise2
+      p.y += numNoise + numNoise2
+    }
+
+    if(p.x > this.style.origin.x && p.y < this.style.origin.y) {
+      p.x -= numNoise + numNoise2
+      p.y -= numNoise + numNoise2
+    }
+
+    if(p.x >= this.style.origin.x && p.y >= this.style.origin.y) {
+      p.x += numNoise + numNoise2
+      p.y -= numNoise + numNoise2
+    }
+
+    if(p.x < this.style.origin.x && p.y > this.style.origin.y) {
+      p.x += numNoise + numNoise2
+      p.y += numNoise + numNoise2
+    }
 
     this.points.push(p);
     ctx.lineTo(p.x, p.y)
   }
+  ctx.lineTo(this.pointB.x, this.pointB.y)
 
   ctx.stroke();
-
-  ctx.beginPath()
-  ctx.fillStyle = "#000";
-  this.points.map(function(point){
-    ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI);
-  })
-  ctx.fill();
 
 }
 
